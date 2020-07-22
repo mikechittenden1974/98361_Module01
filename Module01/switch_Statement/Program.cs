612bc0234715bdd6662a6438cb8c89f07f9cf0da
softwaretrainingatpeta@outlook.com <softwaretrainingatpeta@outlook.com>
2020-07-22 18:50:40 +0100

    CommitSwitch

--- Module01/switch_Statement/Program.cs
+++ Module01/switch_Statement/Program.cs
@@ -6,7 +6,32 @@ namespace switch_Statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestSwitch(10, 20, '+');
+        }
+
+        public static void TestSwitch(int op1, int op2, char opr)
+        {
+            int result;
+            switch(opr)
+            {
+                case '+':
+                    result = op1 + op2;
+                    break;
+                case '-':
+                    result = op1 - op2;
+                    break;
+                case '*':
+                    result = op1 * op2;
+                    break;
+                case '/':
+                    result = op1 / op2;
+                    break;
+                default:
+                    Console.WriteLine("Unknown operator");
+                    return;
+            }
+            Console.WriteLine("Result : {0}", result);
+            return;
         }
     }
 }
