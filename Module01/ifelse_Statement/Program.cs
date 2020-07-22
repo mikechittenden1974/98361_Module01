a2c1c17617352875e6388f1c5e5e0799e08df127
softwaretrainingatpeta@outlook.com <softwaretrainingatpeta@outlook.com>
2020-07-22 18:39:31 +0100

    commit4

--- Module01/ifelse_Statement/Program.cs
+++ Module01/ifelse_Statement/Program.cs
@@ -6,7 +6,27 @@ namespace ifelse_Statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestIfElse(10);
+        }
+        
+        public static void TestIfElse(int n)
+        {
+            if (n <= 10)
+            {
+                Console.WriteLine("n is less than, or equal to 10");
+            }
+            else if (n <= 20)
+            {
+                Console.WriteLine("n is less, or equal to 20");
+            }
+            else if (n <= 30)
+            {
+                Console.WriteLine("n is less, or equal to 30");
+            }
+            else
+            {
+                Console.WriteLine("n is greater than, or equal, to 30");
+            }
         }
     }
 }
