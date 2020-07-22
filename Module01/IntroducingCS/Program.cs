e7deb5b996ced2ec6708a8a7ccd51a82f990b2e7
softwaretrainingatpeta@outlook.com <softwaretrainingatpeta@outlook.com>
2020-07-22 17:09:49 +0100

    First Commit

---
+++ Module01/IntroducingCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroducingCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
