b93beffd439a06315a0082a40402e576d81990ba
softwaretrainingatpeta@outlook.com <softwaretrainingatpeta@outlook.com>
2020-07-22 18:31:04 +0100

    Commit3

--- Module01/IntroducingCS/Program.cs
+++ Module01/IntroducingCS/Program.cs
@@ -7,6 +7,7 @@ namespace IntroducingCS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye, World!");
         }
     }
 }
--- Module01/if_Statement/Program.cs
+++ Module01/if_Statement/Program.cs
@@ -6,7 +6,17 @@ namespace if_Statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number1 = 25;
+            int number2 = 20;
+
+            if (number1 > 5)
+            {
+                Console.WriteLine("number1 is greater than 5");
+                if (number1 < 20)
+                {
+                    Console.WriteLine("number1 is less than 20");
+                }
+            }
         }
     }
 }
